7d9d3732c0150cd004fbac34c945c77d1307479c
kaido <maxartem0419@gmail.com>
2022-04-20 13:16:32 +0300

    fix app module

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use('/t', require('./routes/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
 
 async function start() {
   try {
